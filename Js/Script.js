d3ef6c0e6f83bd7187ea2d8cd24220e97564ea91
Sakshi-Bhatkar <sakshibhatkar88@gmail.com>
2022-08-17 23:09:45 +0530

    Featured books And New Arrivals- (Swiper)

--- Js/Script.js
+++ Js/Script.js
@@ -36,22 +36,71 @@ window.onload = () =>
     }
 }
 
-var swiper= new Swiper(".books-slider", {
-   loop:true,
-   centeredSlides: true,
-   autoplay: {
-    delay:9500,
-    disableOnInteraction: false,
-   },
-   breakpoints: {
-    0: { 
-        slidesPerView: 1,
+var swiper = new Swiper(".books-slider", {
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 9500,
+      disableOnInteraction: false,
     },
-    768:{
-          slidesPerView:2,
-        },
-    1024:{
-           slidesPerView:3,
-        },
-},
-});
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+  var swiper = new Swiper(".featured-slider", {
+    spaceBetween:10,
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 9500,
+      disableOnInteraction: false,
+    },
+    navigation:{
+      nextEl:".swiper-button-next",
+      prevEl:"swiper-button-prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      450: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 4,
+      },
+    },
+  });
+
+  var swiper = new Swiper(".arrivals-slider", {
+    spaceBetween:10,
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 9500,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
